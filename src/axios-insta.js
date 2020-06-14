a18f27b3eac0c91c0f59164c3b8ace1c53702732
shubhamsks <shubhamkumar7051@gmail.com>
2020-06-14 23:35:35 +0530

    https

--- src/axios-insta.js
+++ src/axios-insta.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://instaclone.pythonanywhere.com/api/',
+    baseURL:'https://instaclone.pythonanywhere.com/api/',
 });
 export default instance
